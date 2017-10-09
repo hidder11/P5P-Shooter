3ebfebfd687f31200db26b6075bfd5f86290991c
Aran Kieskamp <aranna00@gmail.com>
2017-10-09 12:15:15 +0200

    base fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,14 @@ var moveUp = false;
 var prevTime = performance.now();
 var velocity = new THREE.Vector3();
 var freeCam = false;
+var geometry, material, mesh;
+var controls;
+var objects = [];
+var raycaster;
 
+var blocker = document.getElementById('blocker');
+var instructions = document.getElementById('instructions');
+var camDirection;
 var geometry, material, mesh;
 var controls;
 var objects = [];
@@ -21,6 +28,50 @@ var raycaster;
 var blocker = document.getElementById('blocker');
 var instructions = document.getElementById('instructions');
 var camDirection;
+
+var havePointerLock = 'pointerLockElement' in document ||
+    'mozPointerLockElement' in document || 'webkitPointerLockElement' in
+    document;
+
+if (havePointerLock) {
+    var element = document.body;
+    var pointerlockchange = function(event) {
+        if (document.pointerLockElement === element ||
+            document.mozPointerLockElement === element ||
+            document.webkitPointerLockElement === element) {
+            controlsEnabled = true;
+            controls.enabled = true;
+            blocker.style.display = 'none';
+        } else {
+            controls.enabled = false;
+            blocker.style.display = '-webkit-box';
+            blocker.style.display = '-moz-box';
+            blocker.style.display = 'box';
+            instructions.style.display = '';
+        }
+    };
+    var pointerlockerror = function(event) {
+        instructions.style.display = '';
+    };
+    // Hook pointer lock state change events
+    document.addEventListener('pointerlockchange', pointerlockchange, false);
+    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+    document.addEventListener('webkitpointerlockchange', pointerlockchange,
+        false);
+    document.addEventListener('pointerlockerror', pointerlockerror, false);
+    document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+    document.addEventListener('webkitpointerlockerror', pointerlockerror,
+        false);
+    instructions.addEventListener('click', function(event) {
+        instructions.style.display = 'none';
+        // Ask the browser to lock the pointer
+        element.requestPointerLock = element.requestPointerLock ||
+            element.mozRequestPointerLock || element.webkitRequestPointerLock;
+        element.requestPointerLock();
+    }, false);
+} else {
+    instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+}
 
 function init() {
     //init scene, camera, lights
@@ -96,12 +147,6 @@ function init() {
         new THREE.Vector3(0, -1, 0), 0, 10);
     //load objects
     var loader = new THREE.ObjectLoader();
-    table = loader.load('assets/models/pool-table.json', function(table) {
-        scene.add(table);
-        table.position.x = -0.002;
-        table.position.y = -0.842;
-        table.position.z = -0.07;
-    });
 
     //add everything to the scene
     scene.add(light, directionalLight);
@@ -131,3 +176,5 @@ function animate() {
     prevTime = time;
     renderer.render(scene, camera);
 }
+
+init();

8b528cf661f4790dbdea879346944d9a2a220aa1
hidder11 <hidder11@gmail.com>
2017-10-17 13:41:37 +0200

    P5P-28 UI crosshair + killfeed

---
+++ assets/js/game.ui.js
@@ -0,0 +1,22 @@
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+
+function showKill(killer, victim, weapon) {
+    toastr.info(killer + '&nbsp;&nbsp;<i class="fa fa-times" aria-hidden="true"></i>&nbsp;&nbsp;' + victim);
+}
+

eec803f1d12eb003e654fe8852a4fe7d6a73ef5c
hidder11 <hidder11@gmail.com>
2017-10-17 17:01:31 +0200

    P5P-28 UI health + ammo bar

--- assets/js/game.ui.js
+++ assets/js/game.ui.js
@@ -16,7 +16,39 @@ toastr.options = {
     "hideMethod": "fadeOut"
 };
 
+var prevHealth = 100;
+
+$('#health').circleProgress({
+    value: 1,
+    size: 80,
+    fill: {
+        color: "#ff402f"
+    }
+});
+
+$('#ammo').circleProgress({
+    value: 0.75,
+    size: 80,
+    fill: {
+        color: "#2e67ff"
+    }
+});
+
 function showKill(killer, victim, weapon) {
     toastr.info(killer + '&nbsp;&nbsp;<i class="fa fa-times" aria-hidden="true"></i>&nbsp;&nbsp;' + victim);
+}
+
+function toggleCrosshair(state) {
+    if (state) {
+        $('#crosshair').show()
+    }
+    else {
+        $('#crosshair').hide()
+    }
+}
+
+function updateHealth(health) {
+    $('#health').circleProgress({animationStartValue: prevHealth, value: health,});
+    prevHealth = health;
 }
 
